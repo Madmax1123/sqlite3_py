7d550dc14d4e8b467f281c9dfb7afed66310ae29
Madmax1123 <79648175+Madmax1123@users.noreply.github.com>
2021-10-29 09:12:32 -0300

    Update python1.py

--- python1.py
+++ python1.py
@@ -9,10 +9,12 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS names(
 	name TEXT
 	
 	);""")
+
+N_name = input('Insert a name:')
 # INSERI VALORES NO DATA
-cursor.execute("INSERT INTO names VALUES('Nome')")
+cursor.execute("INSERT INTO names(name) VALUES(?)", (N_name))
 path_db.commit()
 
 # SELECIONA VALORES DO DATA E COLOCA SEUS ID"NÚMERAÇÃO"
 cursor.execute("SELECT rowid, * FROM names")
-names = cursor.fetchall()
+names = cursor.fetchall() 
